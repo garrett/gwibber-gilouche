b6321771621f51c0aa5e07562c09367dada9f519
Garrett LeSage <garrett@novell.com>
2009-02-25 14:23:50 +0100

    making HTML use [].join('') instead of +=

--- darkilouche.js
+++ darkilouche.js
@@ -33,22 +33,23 @@
   function addMessage(data) {
     var html;
 
-    html = '<div id="'+ data.gId +'" class="message '+ data.username + data.protocol + ' ' + data.aId + data.bgcolor +'">';
-    html += (data.image ? '<a href="' + data.profile_url +'"><span class="imgbox" style="background-image: url('+ data.image +');"></span></a>' : '');
-    html += '<div class="diggbox"></div>';
-    html += '<p class="content">'
-    html += '  <span class="title">' + (data.title == undefined ? data.sender : data.title) + '</span>';
-    html += '  <span class="time"> (<a href="'+ data.url +'">'+ data.time_string +'</a>';
-    html += (data.reply_nick ? ' in reply to <a href="'+ data.reply_url +'">' + data.reply_nick +'</a>' : '');
-    html += ')</span>';
-    html += '<span class="text">'+ data.html_string +'</span>';
-    html += '</p>';
-    html += '<div class="toggledupe"></div>';
-    html += '  <div class="dupes"></div>';
-    html += (data.can_thread ? '<a class="thread replybutton" href="gwibber:thread/' + data.message_index + '"></a>' : '');
-    html += '<a class="reply replybutton" href="gwibber:reply/'+ data.message_index +'"></a>';
-    html += (data.is_unread ? '<div class="unread"></div>' : '');
-    html += '</div>';
+    html = [
+      '<div id="'+ data.gId +'" class="message '+ data.username + data.protocol + ' ' + data.aId + data.bgcolor +'" title="'+ data.sender_nick +'">',
+      (data.image ? '<a href="' + data.profile_url +'"><span class="imgbox" style="background-image: url('+ data.image +');"></span></a>' : ''),
+      '<div class="diggbox"></div>',
+      '<p class="content">',
+      '  <span class="title">' + (data.title == undefined ? data.sender : data.title) + '</span>',
+      '  <span class="time"> (<a href="'+ data.url +'">'+ data.time_string +'</a>',
+      (data.reply_nick ? ' in reply to <a href="'+ data.reply_url +'">' + data.reply_nick +'</a>' : ''),
+      ')</span>',
+      '<span class="text">'+ data.html_string +'</span>',
+      '</p>',
+      '<div class="toggledupe"></div>',
+      '  <div class="dupes"></div>',
+      (data.can_thread ? '<a class="thread replybutton" href="gwibber:thread/' + data.message_index + '"></a>' : ''),
+      '<a class="reply replybutton" href="gwibber:reply/'+ data.message_index +'"></a>',
+      '</div>'
+    ].join('');
 
     if (data.is_duplicate) {
       $("#" + data.gId + " .dupes:first").append(html);
